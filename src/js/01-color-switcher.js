7d5105a101e0f82a988ba96d1314c0c23c9791cc
Oksana-Pak <okspak7@gmail.com>
2022-10-31 12:32:53 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+refs.stop.disabled = true;
+let intervalId;
+
+refs.start.addEventListener('click', onClickBtnStart);
+refs.stop.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart() {
+  refs.stop.disabled = false;
+  refs.start.disabled = true;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickBtnStop() {
+  clearInterval(intervalId);
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
