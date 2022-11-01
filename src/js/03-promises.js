c266f70480ab1234a69601c925c92d6e9650ad84
Oksana-Pak <okspak7@gmail.com>
2022-11-01 14:39:42 +0200

    add task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('form');
+formRef.addEventListener('submit', onBtnClick);
+
+function onBtnClick(e) {
+  e.preventDefault();
+  const formEl = e.target.elements;
+  let delay = Number(formEl.delay.value);
+  let step = Number(formEl.step.value);
+  const amountPromises = Number(formEl.amount.value);
+
+  for (let i = 1; i <= amountPromises; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
